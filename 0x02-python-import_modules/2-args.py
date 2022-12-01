ba99b4f92a6a695bdd327edf8a2bfa58ed409437
Godwin Adah <adahgodwin224@gmail.com>
2022-12-01 09:37:04 -0800

    push

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,12 +4,12 @@ from sys import argv
 n = len(argv)
 index = 1
 if n == 1:
-    print(f"{n-1} argument.")
+    print(f"0 argument.")
 elif n == 2:
-    print(f"{n-1} argument:")
-    print(f"{index}: {argv[index]}")
+    print(f"{n-1:d} argument:")
+    print(f"{index:d}: {argv[index]}")
 elif n > 2:
     print(f"{n-1} arguments:")
     for j in range(1, n):
-        print(f"{index}: {argv[index]}")
+        print(f"{index:d}: {argv[index]}")
         index += 1
