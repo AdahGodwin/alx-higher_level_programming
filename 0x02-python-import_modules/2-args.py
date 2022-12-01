e870aa4b5548a26fe61d53d926f9bee14001d9ee
Godwin Adah <adahgodwin224@gmail.com>
2022-12-01 09:40:00 -0800

    abeg na

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,9 +7,9 @@ if n == 1:
     print(f"0 argument.")
 elif n == 2:
     print(f"{n-1:d} argument:")
-    print(f"{index:d}: {argv[index]}")
+    print(f"{index:d}: {argv[index]:s}")
 elif n > 2:
     print(f"{n-1} arguments:")
     for j in range(1, n):
-        print(f"{index:d}: {argv[index]}")
+        print(f"{index:d}: {argv[index]:s}")
         index += 1
