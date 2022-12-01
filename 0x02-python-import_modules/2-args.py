22a948d9d54f20397e7e72588886ef4424458bce
Godwin Adah <adahgodwin224@gmail.com>
2022-12-01 09:45:01 -0800

    fixed again

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ from sys import argv
 
 if __name__ == '__main__':
     if len(argv) == 1:
-        print('0 arguments.')
+        print('0 argument.')
     elif len(argv) == 2:
         print('{:d} argument:'.format(len(argv) - 1))
     else:
